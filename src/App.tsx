39d3cb168417729018e19308c433b12320c0601f
mc4rvalho <mcarvalho093@gmail.com>
2026-02-26 20:09:14 -0300

    adicionando ScrollToTop, para que a página sempre carregue de cima

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,22 @@ import { AboutPage } from "./pages/AboutPage"
 import { Home } from "./pages/Home"
 import { ContactPage } from "./pages/ContactPage"
 import { MemberPage } from "./pages/MemberPage"
+import { ScrollToTop } from "./components/ScrollToTop"
 
 export function App() {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <Navbar />
         <div className="min-h-[80vh]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/member/:slug" element={<MemberPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/member/:slug" element={<MemberPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
         </div>
         <Footer />
       </BrowserRouter>
